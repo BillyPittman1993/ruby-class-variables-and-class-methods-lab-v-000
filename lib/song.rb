0528b1a01abec7a9aa2f48a583757c339509a2b0
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-07-07 04:02:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,17 @@
 class Song 
   
-  @@count = 0
-  @@artists = []
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0 
+  @@artists = [] 
   @@genres = []
   
-  attr_accessor :name, :artist, :genre 
-  
   def initialize(name, artist, genre)
-    @@count += 1
-    @name = name 
-    @artist = artist 
+    @@count += 1 
+    @name = name
+    @artist = artist
     @genre = genre 
-    @@artists << artist 
+    @@artists << artist
     @@genres << genre 
   end
   
@@ -25,29 +25,11 @@ class Song
   
   def self.genres 
     @@genres.uniq
-  end 
-  
-  def self.genre_count
-    count = {}
-    @@genres.each do |genre| 
-     if count.has_key?(genre)
-      count[genre] += 1 
-    else 
-      count[genre] = 1 
-     end
-    end
-  count 
   end
   
-  def self.artist_count
-    count = {}
-    @@artists.each do |artist| 
-      if count.has_key?(artist)
-      count[artist] += 1
-    else
-      count[artist] = 1
-      end
-    end
-    count 
-  end 
+  def self.genre_count 
+    
+  end
+
 end
+
